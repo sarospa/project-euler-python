8bca4a160e6bc43afadc4427fd8742202717fc08
Christopher Rohrbaugh <ffivfan@hotmail.com>
2019-04-06 19:02:06 -0400

    Added solution 19

---
+++ euler-019.py
@@ -0,0 +1,25 @@
+# Solution to https://projecteuler.net/problem=19
+
+import utilities
+
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def main():
+	day = 1
+	month = 0
+	year = 1901
+	count = 0
+	while year < 2001:
+		days_in_month = months[month]
+		if month == 1 and ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
+			days_in_month += 1
+		day = (day + months[month]) % 7
+		if day == 6:
+			count += 1
+		month += 1
+		if month >= 12:
+			month = 0
+			year += 1
+	print(count)
+
+utilities.print_runtime(main)
