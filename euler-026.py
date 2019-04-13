6e49d99af43a0c902c7d2124d0ac409814a6b375
Christopher Rohrbaugh <ffivfan@hotmail.com>
2019-04-13 14:03:46 -0400

    Added solution 26

---
+++ euler-026.py
@@ -0,0 +1,26 @@
+# Solution to https://projecteuler.net/problem=26
+
+import utilities
+import math
+
+def main():
+	longest_cycle = 0
+	value = 0
+	for i in range(2, 1000):
+		power_of_ten = 10**(int(math.log10(i))+1)
+		remainders = []
+		remainder = power_of_ten % i
+		while remainder not in remainders and remainder != 0:
+			power_of_ten *= 10
+			remainders.append(remainder)
+			remainder = power_of_ten % i
+		if remainder == 0:
+			cycle = 0
+		else:
+			cycle = len(remainders) - remainders.index(remainder)
+		if cycle > longest_cycle:
+			longest_cycle = cycle
+			value = i
+	print(value)
+
+utilities.print_runtime(main)
