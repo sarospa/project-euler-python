1ee3b1f1e276c33006d3e646628fa6ac7d0a5829
Christopher Rohrbaugh <ffivfan@hotmail.com>
2019-04-14 21:50:06 -0400

    Added solution 28

---
+++ euler-028.py
@@ -0,0 +1,13 @@
+# Solution to https://projecteuler.net/problem=28
+
+import utilities
+
+def main():
+	total = 1
+	n = 1
+	for i in range(2, 1001, 2):
+		total += n * 4 + i * 10
+		n += i * 4
+	return total
+
+utilities.print_runtime(main)
