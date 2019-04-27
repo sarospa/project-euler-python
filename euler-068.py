c1f356461e285f4636df14ef9f80288b6d27746b
Christopher Rohrbaugh <ffivfan@hotmail.com>
2019-04-27 18:27:26 -0400

    Added solution 69

--- euler-068.py
+++ euler-068.py
@@ -11,7 +11,7 @@ def main():
 	n_gons = [n_gon + [(pair[0], n_gon[1][2], pair[1])] for n_gon in n_gons for pair in itertools.permutations([num for num in nums if num not in utilities.flatten(n_gon)], 2) if sum(n_gon[0]) == sum((pair[0], n_gon[1][2], pair[1]))]
 	n_gons = [n_gon + [(pair[0], n_gon[2][2], pair[1])] for n_gon in n_gons for pair in itertools.permutations([num for num in nums if num not in utilities.flatten(n_gon)], 2) if sum(n_gon[0]) == sum((pair[0], n_gon[2][2], pair[1]))]
 	n_gons = [n_gon + [(sum(n_gon[0]) - (n_gon[3][2] + n_gon[0][1]), n_gon[3][2], n_gon[0][1])] for n_gon in n_gons if sum(n_gon[0]) - (n_gon[3][2] + n_gon[0][1]) not in utilities.flatten(n_gon) and sum(n_gon[0]) - (n_gon[3][2] + n_gon[0][1]) <= 10 and sum(n_gon[0]) - (n_gon[3][2] + n_gon[0][1]) >= 1]
-	n_gons = sorted([n_gon[n_gon.index(min(n_gon)):] + n_gon[:n_gon.index(min(n_gon))] for n_gon in n_gons if 10 in [triplet[0] for triplet in n_gon]])
+	n_gons = sorted([n_gon[n_gon.index(min(n_gon)):] + n_gon[:n_gon.index(min(n_gon))] for n_gon in n_gons])
 	n_gons = [''.join([''.join([str(n) for n in triplet]) for triplet in n_gon]) for n_gon in n_gons]
 	return n_gons[-1]
 
---
+++ euler-069.py
@@ -0,0 +1,21 @@
+# Solution to https://projecteuler.net/problem=69
+
+import math
+import utilities
+
+def main():
+	utilities.generate_primes_sieve(1000000)
+	totients = list(range(1000001))
+	for p in utilities.primes:
+		for n in range(p, 1000001, p):
+			totients[n] = totients[n] * (p - 1) // p
+	max_ratio = 0
+	best_n = 0
+	for n in range(1, 1000001):
+		if n / totients[n] > max_ratio:
+			max_ratio = n / totients[n]
+			best_n = n
+	return best_n
+
+if __name__ == "__main__":
+	utilities.print_runtime(main)
